dc927ebf6789e463c694ac62c98dbc5a5cd297f3
reneecwl <reneewlcheng@gmail.com>
2025-02-27 13:02:23 -0500

    Add search feature

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,32 @@
 import "./SearchBar.scss";
+import { useState } from "react";
 
-export default function SearchBar() {
+export default function SearchBar({ setSearchQuery }) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setSearchQuery(inputValue);
+  };
+
   return (
     <div className="searchbar">
-      <div>
-        <input
-          type="text"
-          placeholder="Your new best friend is waiting for you..."
-          // value={query}
-          // onChange={handleInputChange}
-        />
-      </div>
+      <input
+        type="text"
+        placeholder="Your new best friend is waiting for you..."
+        className="searchbar__searchbar"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button
+        className="searchbar__button"
+        onClick={handleSearch}
+      >
+        Find my Pawfect Match!
+      </button>
       <h3 className="searchbar__title">Search to find your new best friend!</h3>
     </div>
   );
