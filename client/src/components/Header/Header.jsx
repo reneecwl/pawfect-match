e110b3af2ffb20c9caa18b77cac2a1e3f1af12f5
lmolsen <lmarieolsen@gmail.com>
2025-03-15 13:08:43 -0400

    change title

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import logo from './../../assets/images/logo.png';
 export default function () {
   return (
     <header className="header">
-      <h1 className="header__title">Animal Rescue</h1>
+      <h1 className="header__title">Pawfect Match</h1>
       <img className="header__icon" src={logo} alt="logo"/>
     </header>
   );
